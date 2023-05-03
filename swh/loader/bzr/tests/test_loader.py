SoftwareHeritage/swh-loader-bzr

commit 182754c35307fdcba7a365616abcb879f1767380

loader: Fix mypy errors

swh/loader/bzr/loader.py
@@ -11,18 +11,7 @@ from functools import lru_cache, partial
 import itertools
 import os
 from tempfile import mkdtemp
-from typing import (
-    Any,
-    Dict,
-    Iterator,
-    List,
-    NewType,
-    Optional,
-    Set,
-    Tuple,
-    TypeVar,
-    Union,
-)
+from typing import Any, Dict, Iterator, List, Optional, Set, Tuple, TypeVar, Union
 
 from breezy import errors as bzr_errors
 from breezy import repository, tsort
@@ -31,6 +20,7 @@ from breezy.builtins import cmd_branch, cmd_upgrade
 from breezy.controldir import ControlDir
 from breezy.revision import NULL_REVISION
 from breezy.revision import Revision as BzrRevision
+from breezy.revision import RevisionID as BzrRevisionId
 from breezy.tree import Tree, TreeChange
 
 from swh.loader.core.loader import BaseLoader
@@ -57,8 +47,6 @@ from swh.storage.interface import StorageInterface
 TEMPORARY_DIR_PREFIX_PATTERN = "swh.loader.bzr.from_disk"
 EXTID_TYPE = "bzr-nodeid"
 EXTID_VERSION: int = 1
-
-BzrRevisionId = NewType("BzrRevisionId", bytes)
 
 T = TypeVar("T")
 
@@ -304,7 +292,7 @@ class BazaarLoader(BaseLoader):
             self._repo_directory = self.directory
 
         repo, branch = self.get_repo_and_branch()
-        repository_format = repo._format.as_string()  # lies about being a string
+        repository_format = repo._format.get_format_string()
 
         if not repository_format == expected_repository_format:
             if repository_format in older_repository_formats:
@@ -425,7 +413,11 @@ class BazaarLoader(BaseLoader):
         revision = Revision(
             author=Person.from_fullname(bzr_rev.get_apparent_authors()[0].encode()),
             date=date,
-            committer=Person.from_fullname(bzr_rev.committer.encode()),
+            committer=(
+                Person.from_fullname(bzr_rev.committer.encode())
+                if bzr_rev.committer
+                else None
+            ),
             committer_date=date,
             type=RevisionType.BAZAAR,
             directory=directory,
@@ -548,7 +540,7 @@ class BazaarLoader(BaseLoader):
 
         return from_disk.Content({"sha1_git": content.sha1_git, "perms": perms})
 
-    def _get_bzr_revs_to_load(self) -> List[BzrRevision]:
+    def _get_bzr_revs_to_load(self) -> List[BzrRevisionId]:
         assert self.repo is not None
         self.log.debug("Getting fully sorted revision tree")
         if self.head_revision_id == NULL_REVISION:
@@ -590,7 +582,9 @@ class BazaarLoader(BaseLoader):
             return new_revisions
         return all_revisions
 
-    def _iterate_ancestors(self, revision_id: BzrRevisionId) -> Iterator[BzrRevisionId]:
+    def _iterate_ancestors(
+        self, revision_id: BzrRevisionId
+    ) -> Iterator[Tuple[BzrRevisionId, Tuple[BzrRevisionId]]]:
         """Return an iterator of this revision's ancestors"""
         assert self.repo is not None
         return self.repo.get_graph().iter_ancestry([revision_id])

commit 95936942bfd29332aa218b7d1aeb16643a3ae170

loader: Ensure all committer values are handled in store_revision
It fixes a regression related to empty committer name introduced in 182754c.

It also ensures that null committer is also handled, in that case committer
date must also be set to None.

Fixes #4582

swh/loader/bzr/loader.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022  The Software Heritage developers
+# Copyright (C) 2021-2023  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -409,16 +409,18 @@ class BazaarLoader(BaseLoader):
         timezone = round(int(bzr_rev.timezone) / 60)
         date = TimestampWithTimezone.from_numeric_offset(timestamp, timezone, False)
 
+        committer = (
+            Person.from_fullname(bzr_rev.committer.encode())
+            if bzr_rev.committer is not None
+            else None
+        )
+
         # TODO (how) should we store multiple authors? (T3887)
         revision = Revision(
             author=Person.from_fullname(bzr_rev.get_apparent_authors()[0].encode()),
             date=date,
-            committer=(
-                Person.from_fullname(bzr_rev.committer.encode())
-                if bzr_rev.committer
-                else None
-            ),
-            committer_date=date,
+            committer=committer,
+            committer_date=date if committer is not None else None,
             type=RevisionType.BAZAAR,
             directory=directory,
             message=bzr_rev.message.encode(),

swh/loader/bzr/tests/test_loader.py
@@ -1,11 +1,14 @@
-# Copyright (C) 2022  The Software Heritage developers
+# Copyright (C) 2022-2023  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
+
+from datetime import datetime
 import os
 from pathlib import Path
 
 from breezy.builtins import cmd_uncommit
+from breezy.revision import Revision as BzrRevision
 import pytest
 
 from swh.loader.bzr.loader import BazaarLoader, BzrDirectory
@@ -16,6 +19,7 @@ from swh.loader.tests import (
 )
 from swh.model.from_disk import Content
 from swh.model.hashutil import hash_to_bytes
+from swh.model.model import Directory
 from swh.storage.algos.snapshot import snapshot_get_latest
 
 # Generated repositories:
@@ -428,3 +432,34 @@ def test_incremental_uncommitted_head(swh_storage, datadir, tmp_path):
     # Everything is loaded correctly
     stats = get_stats(swh_storage)
     assert stats == {**expected_stats, "origin_visit": 1 + 1, "snapshot": 1 + 1}
+
+
+@pytest.mark.parametrize("committer", ["John Doe <john.doe@example.org>", "", None])
+def test_store_revision_with_empty_or_none_committer(swh_storage, mocker, committer):
+    repo_url = "https://example.org/bzr"
+    loader = BazaarLoader(swh_storage, repo_url, directory=repo_url)
+
+    mocker.patch.object(
+        loader, "store_directories", return_value=Directory(entries=()).id
+    )
+
+    author = "John Doe <john.doe@example.org>"
+    bzr_rev_id = b"john.doe@example.org-20090420060159-7k8cgljzk05xcm0l"
+    bzr_rev = BzrRevision(revision_id=bzr_rev_id, properties={"author": author})
+    bzr_rev.committer = committer
+    bzr_rev.timestamp = datetime.now().timestamp()
+    bzr_rev.timezone = 0
+    bzr_rev.message = "test"
+
+    loader.store_revision(bzr_rev)
+    loader.flush()
+
+    swh_rev_id = loader._get_revision_id_from_bzr_id(bzr_rev_id)
+    swh_rev = swh_storage.revision_get([swh_rev_id])[0]
+
+    if committer is not None:
+        assert swh_rev.committer.fullname == committer.encode()
+        assert swh_rev.committer_date is not None
+    else:
+        assert swh_rev.committer is None
+        assert swh_rev.committer_date is None